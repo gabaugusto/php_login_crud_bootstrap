01c53ac3c5257bf629e316c955e1d10c46f72486
Gabriel Augusto <gab.augusto@gmail.com>
2020-08-31 08:40:54 -0300

    Update Database

--- includes/alunos.sql
+++ includes/alunos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 30, 2020 at 10:47 AM
+-- Generation Time: Aug 31, 2020 at 11:39 AM
 -- Server version: 8.0.18
 -- PHP Version: 7.3.12
 
@@ -39,16 +39,19 @@ CREATE TABLE IF NOT EXISTS `alunos` (
   `fone` char(14) NOT NULL,
   `data_nascimento` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `alunos`
 --
 
 INSERT INTO `alunos` (`id`, `status`, `cpf`, `nome`, `email`, `password`, `fone`, `data_nascimento`) VALUES
-(3, '1', '25589744123', 'Elisabete Ferreira', 'a@b.com', 'banana', '551198765873', '2020-05-25'),
-(5, '2', '05896332104', 'karla Dantas', 'k@dantas.com', 'carrots', '55119875252201', '2020-05-25'),
-(6, '1', '88754215936', 'Jaime Sato', 'adobe@be.com', 'limes', '551198765873', '2020-05-25');
+(5, '2', '05896332104', 'Karla Dantas', 'k@dantas.com', 'carrots', '55119875252201', '2020-05-25'),
+(6, '1', '88754215936', 'Jaime Sato', 'adobe@be.com', 'limes', '551198765873', '2020-05-25'),
+(38, '2', '85696375342', 'Carlos Santos ', 'car.santos@obol.com', 'melancia', '551389638541', '1982-02-25'),
+(37, '1', '2455869630', 'Gabriel Teixeira Azevedo', 'gab.text@gmail.com', '123456', '5511875349621', '1971-03-05'),
+(40, '2', '53698574156', '', 'a@b.com', 'abacaxi', '5511 42369875', '1980-12-25'),
+(39, '2', '53698574156', '', 'a@b.com', 'abacaxi', '5511 42369875', '1980-12-25');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
